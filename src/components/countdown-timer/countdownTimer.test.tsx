eadef60f839a6a779370424ea20e948144e1c1f5
Channa Korach <channa53@gmail.com>
2019-02-11 15:34:18 +0200

    Unit test - countdown timer

--- src/components/countdown-timer/countdownTimer.test.tsx
+++ src/components/countdown-timer/countdownTimer.test.tsx
@@ -2,33 +2,29 @@ import { render } from "preact-render-to-string";
 import { CountdownTimer } from "./CountdownTimer";
 import { h } from "preact";
 
-describe("Countdown timer start", () => {
-    it("Renders without crashing", () => {
-        const div = document.createElement("div");
-        render(
-            <CountdownTimer
-                initialValue={0}
-                onCountdownComplete={() => {
-                    return;
-                }}
-            />,
-            div
-        );
-    });
+it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    render(
+        <CountdownTimer
+            initialValue={3}
+            onCountdownComplete={() => {
+                return;
+            }}
+        />,
+        div
+    );
 });
 
-describe("Countdown timer snapshot", () => {
-    it("should render header with content", () => {
-        const tree = render(
-            <CountdownTimer
-                initialValue={0}
-                onCountdownComplete={() => {
-                    return;
-                }}
-            />
-        );
-        expect(tree).toMatchSnapshot();
-    });
+it("Match snapshot", () => {
+    const tree = render(
+        <CountdownTimer
+            initialValue={3}
+            onCountdownComplete={() => {
+                return;
+            }}
+        />
+    );
+    expect(tree).toMatchSnapshot();
 });
 
 console.log("I (test) render");
